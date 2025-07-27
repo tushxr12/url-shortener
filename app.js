c662cb425a6443421621e8092da15a2dd16625ac
Tushar <tusharforcourses@gmail.com>
2025-07-27 18:50:30 +0530

    change port to 3000

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const urlRoutes = require('./routes/urlRoutes');
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use("/" , urlRoutes);
 
 dotenv.config();
 
-app.use(express.json());
+// app.use((req, res, next) => {
+//   console.log(`[${req.method}] ${req.url}`);
+//   next();
+// });
 
-app.get('/', (req, res) => {
-  res.send('URL shortener API is running');
-});
+// app.get('/', (req, res) => {
+//   res.send('URL shortener API is running');
+// });
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
     .then(() => {
         console.log('Connected to MongoDB');
         app.listen(PORT, () => {
